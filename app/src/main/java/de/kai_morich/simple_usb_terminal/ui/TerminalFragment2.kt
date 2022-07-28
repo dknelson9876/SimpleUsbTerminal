5c302914b6dfc3ab0e3c0caed60a002d17662dfa
dknelson9876 <dknelson9876@gmail.com>
2022-07-28 11:06:30 -0600

    Continue adding to TerminalViewModel

--- app/src/main/java/de/kai_morich/simple_usb_terminal/ui/TerminalFragment2.kt
+++ app/src/main/java/de/kai_morich/simple_usb_terminal/ui/TerminalFragment2.kt
@@ -3,18 +3,32 @@ package de.kai_morich.simple_usb_terminal.ui
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.content.IntentFilter
 import android.hardware.usb.UsbManager
 import android.os.Bundle
+import android.os.SystemClock
+import android.text.method.ScrollingMovementMethod
+import android.view.*
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.CompoundButton
+import androidx.appcompat.widget.SwitchCompat
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import com.google.android.gms.location.Priority
+import de.kai_morich.simple_usb_terminal.*
 import de.kai_morich.simple_usb_terminal.databinding.FragmentTerminalBinding
-import de.kai_morich.simple_usb_terminal.Constants
+import de.kai_morich.simple_usb_terminal.services.FirebaseService
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
 
 class TerminalFragment2 : Fragment() {
 
+    private var truncate: Boolean = true
     private val viewModel: TerminalViewModel by viewModels()
 
     private lateinit var binding: FragmentTerminalBinding
+    private var initialStart = true
 
     private val broadcastReceiver = object : BroadcastReceiver () {
         override fun onReceive(context: Context, intent: Intent) {
@@ -36,12 +50,146 @@ class TerminalFragment2 : Fragment() {
 
     override fun onDestroy() {
         if(viewModel.connected != TerminalViewModel.Connected.False)
-            disconnect()
+            viewModel.disconnect()
         requireActivity().unbindService(viewModel)
 
         super.onDestroy()
     }
 
-    //endregion
+    override fun onStart() {
+        super.onStart()
+        //if attach() returns false, the service doesn't exist yet,
+        // so start it and try again
+        if(!viewModel.attach()){
+            requireActivity().startService(Intent(activity, SerialService::class.java))
+            viewModel.attach()
+        }
+    }
+
+    override fun onStop() {
+        viewModel.detach()
+        super.onStop()
+    }
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        requireActivity().bindService(Intent(activity, SerialService::class.java), viewModel, Context.BIND_AUTO_CREATE)
+    }
+
+    override fun onDetach() {
+        requireActivity().unbindService(viewModel)
+        super.onDetach()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        requireActivity().registerReceiver(broadcastReceiver, IntentFilter(Constants.INTENT_ACTION_GRANT_USB))
+        if(initialStart) {
+            initialStart = false
+            requireActivity().runOnUiThread(viewModel.connect)
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_terminal, container, false)
+        binding.receiveText.setTextColor(resources.getColor(R.color.colorRecieveText))
+        binding.receiveText.movementMethod = ScrollingMovementMethod.getInstance()
+
+        binding.setupBtn.setOnClickListener { onSetupClicked() }
+
+        binding.stopUploadBtn.setOnClickListener {
+            val stopIntent = Intent(context, FirebaseService.ActionListener::class.java)
+            stopIntent.action = FirebaseService.KEY_NOTIFICATION_STOP_ACTION
+            stopIntent.putExtra(FirebaseService.KEY_NOTIFICATION_ID, ServiceNotification.notificationId)
+            FirebaseService.getServiceInstance().sendBroadcast(stopIntent)
+        }
+
+        binding.stopMotorBtn.setOnCheckedChangeListener { _, isChecked ->
+            val stopMotorIntent = Intent(context, SerialService.ActionListener::class.java)
+            stopMotorIntent.action = SerialService.KEY_STOP_MOTOR_ACTION
+            stopMotorIntent.putExtra(SerialService.KEY_MOTOR_SWITCH_STATE, isChecked)
+            SerialService.getInstance().sendBroadcast(stopMotorIntent)
+        }
+
+        binding.startBtn.setOnClickListener { send(BGapi.SCANNER_START) }
+        binding.stopBtn.setOnClickListener { send(BGapi.SCANNER_STOP) }
+
+        val gps_options = listOf("Power Saving", "Balanced", "High Accuracy")
+        val adapter: ArrayAdapter<CharSequence> = ArrayAdapter(requireContext(), android.R.layout.simple_spinner_item, gps_options)
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        binding.gpsPrioritySpinner.adapter = adapter
+        binding.gpsPrioritySpinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                val activity = requireActivity()
+                if(activity is MainActivity) {
+                    when (position) {
+                        0 -> activity.updateLocationPriority(Priority.PRIORITY_LOW_POWER)
+                        1 -> activity.updateLocationPriority(Priority.PRIORITY_BALANCED_POWER_ACCURACY)
+                        2 -> activity.updateLocationPriority(Priority.PRIORITY_HIGH_ACCURACY)
+                    }
+                }
+            }
+
+            override fun onNothingSelected(p0: AdapterView<*>?) {
+                // do nothing
+            }
+
+        }
+
+        binding.receiveText.append("Writing to ${LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd_HH_mm_ss"))}_log.txt\n")
+
+        return view
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.menu_terminal, menu)
+        menu.findItem(R.id.truncate).isChecked = truncate
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.clear -> {
+                binding.receiveText.text = ""
+                return true
+            }
+            R.id.manualUpload -> {
+                FirebaseService.getServiceInstance().uploadLog()
+                return true
+            }
+            R.id.truncate -> {
+                truncate = !truncate
+                item.isChecked = truncate
+                return true
+            }
+            R.id.manualCW -> {
+                send(BGapi.ROTATE_CW)
+                SystemClock.sleep(500)
+                send(BGapi.ROTATE_STOP)
+                return true
+            }
+            R.id.manualCCW -> {
+                send(BGapi.ROTATE_CCW)
+                SystemClock.sleep(500)
+                send(BGapi.ROTATE_STOP)
+                return true
+            }
+            else -> return super.onOptionsItemSelected(item)
+        }
+    }
+
+    //endregion Lifecycle
+
+
+
+    private fun onSetupClicked(){
+        send(BGapi.SCANNER_SET_MODE)
+        send(BGapi.SCANNER_SET_TIMING)
+        send(BGapi.CONNECTION_SET_PARAMETERS)
+    }
 }
+
 
--- app/src/main/java/de/kai_morich/simple_usb_terminal/ui/TerminalViewModel.kt
+++ app/src/main/java/de/kai_morich/simple_usb_terminal/ui/TerminalViewModel.kt
@@ -1,16 +1,28 @@
 package de.kai_morich.simple_usb_terminal.ui
 
+import android.app.PendingIntent
 import android.content.ComponentName
+import android.content.Intent
 import android.content.ServiceConnection
+import android.graphics.Color
+import android.hardware.usb.UsbDevice
+import android.hardware.usb.UsbManager
 import android.os.Build
 import android.os.IBinder
+import android.text.Spannable
+import android.text.SpannableStringBuilder
+import android.text.style.ForegroundColorSpan
+import android.widget.Toast
 import androidx.annotation.RequiresApi
+import androidx.core.text.set
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
-import de.kai_morich.simple_usb_terminal.SerialListener
-import de.kai_morich.simple_usb_terminal.SerialService
+import com.hoho.android.usbserial.driver.SerialTimeoutException
+import com.hoho.android.usbserial.driver.UsbSerialPort
+import com.hoho.android.usbserial.driver.UsbSerialProber
+import de.kai_morich.simple_usb_terminal.*
 import java.lang.Exception
 
 class TerminalViewModel(
@@ -27,9 +39,154 @@ class TerminalViewModel(
     private val portNum: Int? = state["port"]
     private val baudRate: Int? = state["baud"]
 
+    private val serialListener: SerialListener = TerminalSerialListener()
+    private lateinit var usbSerialPort: UsbSerialPort
+
+    private var pendingPacket: BlePacket? = null
+
     enum class Connected {False, Pending, True}
 
+    fun attach(): Boolean {
+        return if(serviceConnection.service != null){
+            serviceConnection.service?.attach((serialListener))
+            true
+        } else {
+            false
+        }
+    }
 
+    fun detach() {
+        serviceConnection.service?.detach()
+    }
+
+    fun connect(usbManager: UsbManager) = connect(null)
+
+    fun connect(permissionGranted: Boolean?, usbManager: UsbManager){
+        var device: UsbDevice? = null
+        for(v: UsbDevice in usbManager.deviceList.values)
+            if(v.deviceId == deviceId)
+                device = v
+        if(device == null) {
+            status("connection failed: device not found")
+            return
+        }
+        var driver = UsbSerialProber.getDefaultProber().probeDevice(device)
+        if(driver == null){
+            driver = CustomProber.getCustomProber().probeDevice(device)
+        }
+        if(driver == null){
+            status("connection failed: no driver for device")
+            return
+        }
+        if(driver.ports.size < portNum!!) {
+            status("connection failed: not enough ports at device")
+            return
+        }
+
+        usbSerialPort = driver.ports[portNum]
+        val usbConnection = usbManager.openDevice(driver.device)
+        if(usbConnection == null && permissionGranted == null && !usbManager.hasPermission(driver.device)) {
+            val usbPermissionIntent = PendingIntent.getBroadcast(
+                activity,
+                0,
+                Intent(Constants.INTENT_ACTION_GRANT_USB),
+                PendingIntent.FLAG_IMMUTABLE
+            )
+            usbManager.requestPermission(driver.device, usbPermissionIntent)
+            return
+        }
+        if(usbConnection == null){
+            if(!usbManager.hasPermission(driver.device))
+                status("connection failed: permission denied")
+            else
+                status("connection failed: open failed")
+            return
+        }
+
+        connected = Connected.Pending
+        try {
+            usbSerialPort.open(usbConnection)
+            usbSerialPort.setParameters(baudRate!!, UsbSerialPort.DATABITS_8, UsbSerialPort.STOPBITS_1, UsbSerialPort.PARITY_NONE)
+            val socket = SerialSocket(activity.applicationContext, usbConnection, usbSerialPort)
+            serviceConnection.service?.connect(socket)
+            onSerialConnect()
+        } catch (e: Exception) {
+            onSerialConnectError(e)
+        }
+
+    }
+
+    fun disconnect() {
+        connected = Connected.False
+        serviceConnection.service?.disconnect()
+        usbSerialPort = null
+    }
+
+    fun send(str: String){
+        if(connected != Connected.True){
+            Toast.makeText(activity, "not connected", Toast.LENGTH_SHORT).show()
+            return
+        }
+        try {
+            val sb = StringBuilder()
+            TextUtil.toHexString(sb, TextUtil.fromHexString(str))
+            TextUtil.toHexString(sb, newline.getBytes())
+            var msg = sb.toString()
+            var data: ByteArray = TextUtil.fromHexString(msg)
+            if(BGapi.isCommand(str)){
+                msg = BGapi.getCommandName(str)
+            }
+            val spn = SpannableStringBuilder(msg + "\n")
+            spn.setSpan(ForegroundColorSpan(getResources.getColor(R.color.colorSendText)), 0, spn.length, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
+            _terminalText.value += spn
+            serviceConnection.service?.write(data)
+        } catch (e: SerialTimeoutException){
+            status("write timeout: ${e.message}")
+        } catch (e: Exception) {
+            onSerialIoError(e)
+        }
+    }
+
+    fun receive(data: ByteArray){
+        if(BGapi.isScanReportEvent(data)) {
+            if(pendingPacket != null){
+                var msg = pendingPacket.toString()
+                if(truncate) {
+                    var length = msg.length
+                    if(length > msg.lastIndexOf('\n') + 40 ){
+                        length = msg.lastIndexOf('\n') + 40
+                    }
+                    msg = msg.substring(0, length) + "..."
+                }
+                val spn = SpannableStringBuilder(msg + "\n\n")
+                spn.setSpan(ForegroundColorSpan(Color.MAGENTA), 0, spn.length, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
+                _terminalText.value += spn
+            }
+            if(data.size <= 21)
+                return
+
+            pendingPacket = BlePacket.parsePacket(data)
+        } else if(BGapi.isKnownResponse(data)) {
+            val rsp = BGapi.getResponseName(data)
+            if(rsp != null && !rsp.contains("rotate"))
+                _terminalText.value += rsp + "\n"
+        } else {
+            //until that data has a terminator, assume packets that aren't a known header are incomplete packets
+            if(pendingPacket != null)
+                pendingPacket!!.appendData(data)
+        }
+
+        //if terminalText is getting too large to be reasonable, cut if off
+        if(_terminalText.value!!.length > 8000) {
+            val text = _terminalText.value!!
+            val length = text.length
+            _terminalText.value = text.substring(length-2000, length)
+        }
+    }
+
+    fun status(str: String){
+        //TODO: make this work
+    }
 }
 
 //TODO: this part definitely needs to be rewritten - should be using a repository as a single source of truth
