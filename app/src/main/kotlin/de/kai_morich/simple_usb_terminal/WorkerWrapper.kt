47c60b9a47b8123d3b07991355a839c00ca125dc
cobysandholtz <cobys@byu.edu>
2023-05-03 15:41:25 -0600

    Put the periodic Worker thing in

--- app/src/main/kotlin/de/kai_morich/simple_usb_terminal/WorkerWrapper.kt
+++ app/src/main/kotlin/de/kai_morich/simple_usb_terminal/WorkerWrapper.kt
@@ -1,10 +1,8 @@
 package de.kai_morich.simple_usb_terminal
 
 import android.content.Context
-import androidx.work.Constraints
-import androidx.work.OneTimeWorkRequestBuilder
-import androidx.work.OutOfQuotaPolicy
-import androidx.work.WorkManager
+import androidx.work.*
+import java.util.concurrent.TimeUnit
 
 /**
  * A wrapper class that makes it easier to start a given service
@@ -13,11 +11,15 @@ class WorkerWrapper {
     companion object {
         @JvmStatic fun startFirebaseWorker(context: Context){
             val constraints = Constraints.Builder().build()
-            val request = OneTimeWorkRequestBuilder<FirebaseWorker>()
-                .setExpedited(OutOfQuotaPolicy.RUN_AS_NON_EXPEDITED_WORK_REQUEST)
-                .setConstraints(constraints)
-                .build()
-            WorkManager.getInstance(context).enqueue(request)
+//            val request = OneTimeWorkRequestBuilder<FirebaseWorker>()
+//                .setExpedited(OutOfQuotaPolicy.RUN_AS_NON_EXPEDITED_WORK_REQUEST)
+//                .setConstraints(constraints)
+//                .build()
+//            WorkManager.getInstance(context).enqueue(request)
+            val periodicWorker = PeriodicWorkRequestBuilder<FirebaseWorker>(15,
+                    TimeUnit.MINUTES).build()
+            WorkManager.getInstance(context).enqueueUniquePeriodicWork(
+                    "FirebaseService", ExistingPeriodicWorkPolicy.KEEP, periodicWorker)
         }
         @JvmStatic fun startSerialWorker(context: Context){
             val constraints = Constraints.Builder().build()
